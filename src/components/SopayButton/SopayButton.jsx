4562c3988ee9ea4b41573b5101089a7c9fafb533
Tchaya Yvan <tchayayvan76@gmail.com>
2023-06-12 13:33:05 +0200

    commit

--- src/components/SopayButton/SopayButton.jsx
+++ src/components/SopayButton/SopayButton.jsx
@@ -1,9 +1,5 @@
 import React, { useEffect } from "react";
 
-export const SopayButton = () => {
-  return <div className="sopayButton"></div>;
-};
-
 export const useSopayButton = ({
   apiKey,
   langue,
@@ -18,19 +14,14 @@ export const useSopayButton = ({
   successUrl,
 }) => {
   useEffect(() => {
-    const script1 = document.createElement("script");
-    const script2 = document.createElement("script");
-    script1.id = "SBScript";
-    script1.type = "text/javascript";
-    script1.setAttribute("data-lang", langue);
-    script1.setAttribute("data-apikey", apiKey);
-    script1.async = true;
-    script1.src = "https://soleaspay.com/webPay/v2/sopayButton.min.js";
-    document.body.appendChild(script1);
-    script1.onload = () => {
-      script2.type = "text/javascript";
-      script2.async = false;
-      script2.innerHTML = `
+
+    const scriptExist1 = document.getElementById('SBScript');
+    const scriptExist2 = document.getElementById('SBScript2');
+
+    if (scriptExist1 && scriptExist2) {
+      scriptExist1.setAttribute("data-lang", langue);
+      scriptExist1.setAttribute("data-apikey", apiKey);
+      scriptExist2.innerHTML = `
       const options = {
         btnTitle: '${btnTitle}',
         amount: ${amount},
@@ -55,16 +46,49 @@ export const useSopayButton = ({
 
       initButton();
     `;
-      document.body.appendChild(script2);
-    };
 
+    } else {
+      const script1 = document.createElement("script");
+      const script2 = document.createElement("script");
+      script1.id = "SBScript";
+      script1.type = "text/javascript";
+      script1.setAttribute("data-lang", langue);
+      script1.setAttribute("data-apikey", apiKey);
+      script1.async = true;
+      script1.src = "https://soleaspay.com/webPay/v2/sopayButton.min.js";
+      document.body.appendChild(script1);
+      script1.onload = () => {
+        script2.id = "SBScript2";
+        script2.type = "text/javascript";
+        script2.async = false;
+        script2.innerHTML = `
+      const options = {
+        btnTitle: '${btnTitle}',
+        amount: ${amount},
+        currency: "${currency}",
+        orderId: "${orderId}",
+        description: "${description}",
+        businessName: '${businessName}',
+        loadInvoice: ${loadInvoice},
+        successUrl: '${successUrl}'
+      };
 
+      function initButton() {
+        return SopayButton.${mode}(options)
+        .then((res) => {
+            localStorage.setItem('sopayresult', JSON.stringify(res));
+        })
+        .catch((err) => {
+            localStorage.setItem('sopayresult', JSON.stringify(err));
+        })
+        .finally(initButton);
+      }
 
-    return () => {
-      document.body.removeChild(script2);
-      document.body.removeChild(script1);
-    };
-
+      initButton();
+    `;
+        document.body.appendChild(script2);
+      };
+    }
   }, [
     apiKey,
     langue,
@@ -92,5 +116,6 @@ export const useSopayButton = ({
 
 };
 
-
-
+export const SopayButton = () => {
+  return <div className="sopayButton"></div>;
+};
